4ba0398e1199ebc32c565f745b92fb45a7761c7a
danqing <danqing.wen@qq.com>
2018-12-09 11:50:54 +0800

    add sleuth[tracing] to see the tracing log.

--- product-consumer/src/main/java/com/danqing/ProductConsumerApplication.java
+++ product-consumer/src/main/java/com/danqing/ProductConsumerApplication.java
@@ -5,10 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
 @EnableCircuitBreaker
 @SpringBootApplication
 @EnableFeignClients()
 @EnableEurekaClient
+@EnableHystrixDashboard
 public class ProductConsumerApplication {
 
     public static void main(String[] args) {
